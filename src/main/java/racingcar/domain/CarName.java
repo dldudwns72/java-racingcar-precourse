2f869f7cf64123bec2533c1b6b17672c043998a8
dldudwns72 <dldudwns72@gmail.com>
2022-04-20 01:34:02 +0900

    feat(CarName) : 자동차 이름 정보 및 예외 구현

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,2 +1,24 @@
-package racingcar.domain;public class CarName {
+package racingcar.domain;
+
+public class CarName {
+
+    private static final Integer MIN_CAR_NAME_LENGTH = 1;
+    private static final Integer MAX_CAR_NAME_LENGTH = 5;
+
+    private String name;
+
+    public CarName(String name){
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if(name.length() < MIN_CAR_NAME_LENGTH || name.length() > MAX_CAR_NAME_LENGTH ) {
+            throw new IllegalArgumentException("[ERROR] 이름은 1자 이상 5자 미만이여야 합니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
 }
