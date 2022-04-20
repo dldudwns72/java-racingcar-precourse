e795ee6386baa408a0212125c34332c647e56544
dldudwns72 <dldudwns72@gmail.com>
2022-04-20 21:38:39 +0900

    test(CarPosition) : 자동차 위치 정보(CarPosition) 테스트

--- src/test/java/racingcar/domain/CarPositionTest.java
+++ src/test/java/racingcar/domain/CarPositionTest.java
@@ -1,4 +1,26 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class CarPositionTest {
-  
+
+    @Test
+    void createCarPosition() {
+        CarPosition carPosition = new CarPosition(3);
+        assertThat(carPosition.getPosition()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("위치값이 0보다 작을 경우 예외 발생")
+    void validatePositionUnderZero() {
+        assertThatThrownBy(() -> {
+            new CarPosition(-1);
+        }).isInstanceOf(IllegalArgumentException.class)
+          .hasMessageContaining("후진");
+    }
+
 }
