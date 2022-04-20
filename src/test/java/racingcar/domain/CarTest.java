75cd22c57c830cbed52b06808d50c76e9b454053
dldudwns72 <dldudwns72@gmail.com>
2022-04-20 22:08:15 +0900

    test(Car) : Car 클래스 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarTest {
-  
+
+    @Test
+    @DisplayName("자동차 정보 반환")
+    void getCarData(){
+        Car car = new Car(new CarName("tesla"),new CarPosition(1));
+
+        assertThat(car.getName()).isEqualTo("tesla");
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 }
