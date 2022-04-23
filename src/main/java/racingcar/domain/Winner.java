8e518b2d26b8658f4967f58538027ec675611607
dldudwns72 <dldudwns72@gmail.com>
2022-04-23 22:33:20 +0900

    feat(Winner) : 우승자 도메인 Winner 구현

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -1,2 +1,23 @@
-package racingcar.domain;public class Winner {
+package racingcar.domain;
+
+public class Winner {
+
+    private static final String WINNER_EMPTY_ERROR_MESSAGE = "[ERROR] 우승자는 한 명 이상 존재해야 합니다.";
+    private String winner;
+
+    public Winner(String winner) {
+        validate(winner);
+        this.winner = winner;
+    }
+
+    private void validate(String winner) {
+        if(winner.isEmpty()){
+            throw new IllegalArgumentException(WINNER_EMPTY_ERROR_MESSAGE);
+        }
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
 }
