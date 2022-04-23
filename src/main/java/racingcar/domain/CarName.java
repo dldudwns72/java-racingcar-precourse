078386a3f0c4a189922676f0b795156566da958b
dldudwns72 <dldudwns72@gmail.com>
2022-04-23 22:13:20 +0900

    feat(CarName) : 자동차 이름 도메인 구현

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -5,6 +5,8 @@ public class CarName {
     private static final Integer MIN_CAR_NAME_LENGTH = 1;
     private static final Integer MAX_CAR_NAME_LENGTH = 5;
 
+    private static final String ERROR_NAME_LENGTH_MESSAGE = "[ERROR] 이름은 1자 이상 5자 미만이여야 합니다.";
+
     private String name;
 
     public CarName(String name){
@@ -14,7 +16,7 @@ public class CarName {
 
     private void validate(String name) {
         if(name.length() < MIN_CAR_NAME_LENGTH || name.length() > MAX_CAR_NAME_LENGTH ) {
-            throw new IllegalArgumentException("[ERROR] 이름은 1자 이상 5자 미만이여야 합니다.");
+            throw new IllegalArgumentException(ERROR_NAME_LENGTH_MESSAGE);
         }
     }
 
