2d075de17fefc44b02d48004e7174f1723b7ddde
dldudwns72 <dldudwns72@gmail.com>
2022-04-23 22:16:12 +0900

    feat(CarPosition) : 자동차 위치 도메인 구현

--- src/main/java/racingcar/domain/CarPosition.java
+++ src/main/java/racingcar/domain/CarPosition.java
@@ -1,7 +1,9 @@
 package racingcar.domain;
 
 public class CarPosition {
-    private Integer position;
+    private static final String PRINT_POSITION_WORD = "-";
+    private static final String MIN_POSITION_ERROR_MESSAGE = "[ERROR] 자동차는 후진할 수 없습니다.";
+    private int position;
 
     public CarPosition(Integer position){
         validate(position);
@@ -10,11 +12,24 @@ public class CarPosition {
 
     private void validate(Integer position) {
         if(position < 0){
-            throw new IllegalArgumentException("자동차는 후진할 수 없습니다.");
+            throw new IllegalArgumentException(MIN_POSITION_ERROR_MESSAGE);
         }
     }
 
-    public Integer getPosition() {
+    public void addPosition(){
+        position++;
+    }
+
+    public String printPosition(){
+        StringBuilder sb = new StringBuilder();
+        for(int index=0;index < position; index++){
+            sb.append(PRINT_POSITION_WORD);
+        }
+
+        return sb.toString();
+    }
+
+    public int getPosition() {
         return position;
     }
 }
