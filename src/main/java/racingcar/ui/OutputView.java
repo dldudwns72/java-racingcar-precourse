4d2bb01985f5a9a27bbf5c56bb35f2b195c61c6c
dldudwns72 <dldudwns72@gmail.com>
2022-04-24 01:02:39 +0900

    feat(Output UI) : 레이싱 결과를 보여주는 OutputView UI

--- src/main/java/racingcar/ui/OutputView.java
+++ src/main/java/racingcar/ui/OutputView.java
@@ -1,2 +1,17 @@
-package racingcar.ui;public class OutputView {
+package racingcar.ui;
+
+import racingcar.domain.Cars;
+import racingcar.domain.Winners;
+
+public class OutputView {
+
+    private static final String PRINT_WINNERS_PREFIX = "최종 우승자는 ";
+    private static final String PRINT_WINNERS_SUFFIX = " 입니다.";
+
+    public static void finishRacing(Cars cars) {
+        Winners winners = new Winners(cars);
+        String winnerToString = winners.printWinners();
+        String printWinners = PRINT_WINNERS_PREFIX + winnerToString + PRINT_WINNERS_SUFFIX;
+        System.out.print(printWinners);
+    }
 }
