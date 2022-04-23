c1b4d0f92a617e05d67d45bba8721e16a129c80d
dldudwns72 <dldudwns72@gmail.com>
2022-04-23 22:19:38 +0900

    feat(Car) : 자동차 전진, 정보 출력, 예외발생 도메인 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,20 +2,45 @@ package racingcar.domain;
 
 public class Car {
 
+    private static int FORWARD_COUNT = 4;
+    private static int START_POSITION_COUNT = 0;
+    private static String PRINT_KEYWORD = "-";
+
     private CarName name;
     private CarPosition position;
 
-    public Car(CarName name, CarPosition position) {
-        this.name = name;
-        this.position = position;
+    public Car(CarName name){
+        this.name= name;
+        this.position = new CarPosition(START_POSITION_COUNT);
     }
 
     public String getName() {
         return name.getName();
     }
 
-    public Integer getPosition() {
+    public int getPosition() {
         return position.getPosition();
     }
 
+    public void move(int number) {
+        if (number >= FORWARD_COUNT) {
+            position.addPosition();
+        }
+    }
+
+    public boolean isEqual(Car car) {
+        return getName().equals(car.getName());
+    }
+
+    public String print() {
+        return getName() + " : " + positionPrint();
+    }
+
+    private String positionPrint(){
+        StringBuilder sb = new StringBuilder();
+        for(int index = 0 ; index < getPosition(); index ++ ){
+            sb.append(PRINT_KEYWORD);
+        }
+        return sb.toString();
+    }
 }
