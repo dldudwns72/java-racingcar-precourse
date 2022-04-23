e4aaee5c3107ac65f94df21e81edf0277be25cdf
dldudwns72 <dldudwns72@gmail.com>
2022-04-24 00:57:38 +0900

    feat(Winners) : 우승자들 도메인 구현

--- src/main/java/racingcar/domain/Winners.java
+++ src/main/java/racingcar/domain/Winners.java
@@ -1,2 +1,54 @@
-package racingcar;public class Winners {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winners {
+
+    private static final String DELIMITER = ",";
+
+    private Cars cars;
+    private List<Winner> winners;
+
+    public Winners(Cars racingFinishCars) {
+        winners = new ArrayList<>();
+        this.cars = racingFinishCars;
+        determineWinner();
+    }
+
+    private void determineWinner() {
+        int maxMoveCount = getMaxMoveCount();
+
+        for (int index = 0; index < cars.getSize(); index++) {
+            Car car = cars.getCar(index);
+            comparePosition(car, maxMoveCount);
+        }
+    }
+
+    private int getMaxMoveCount() {
+        int winnerMoveCount = 0;
+        for (int index = 0; index < cars.getSize(); index++) {
+            Car car = cars.getCar(index);
+            winnerMoveCount = Math.max(winnerMoveCount, car.getPosition());
+        }
+        return winnerMoveCount;
+    }
+
+    private void comparePosition(Car car, int maxMoveCount) {
+        if (car.getPosition() == maxMoveCount) {
+            winners.add(new Winner(car.getName()));
+        }
+    }
+
+    public String printWinners(){
+        List<String> stringWinner = new ArrayList<>();
+        for (Winner winner : winners){
+            stringWinner.add(winner.getWinner());
+        }
+
+        String message = String.join(DELIMITER, stringWinner);
+
+        return message;
+    }
+
 }
