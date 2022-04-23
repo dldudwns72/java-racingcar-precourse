2d8ed76f469a8d7be5293500ebf5ed48111b6bae
dldudwns72 <dldudwns72@gmail.com>
2022-04-23 22:34:08 +0900

    test(Winner) : Winner 도메인 테스트

--- src/test/java/racingcar/domain/WinnerTest.java
+++ src/test/java/racingcar/domain/WinnerTest.java
@@ -1,2 +1,27 @@
-package racingcar.domain;public class WinnerTest {
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinnerTest {
+
+    @Test
+    @DisplayName("우승자 이름 확인")
+    void getWinner(){
+        Winner winner = new Winner("lee");
+        assertThat(winner.getWinner()).isEqualTo("lee");
+    }
+
+    @Test
+    @DisplayName("우승자가 없으면 예외 발생")
+    void emptyWinner(){
+        assertThatThrownBy(() ->{
+            Winner winner = new Winner("");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("우승자는 한 명 이상 존재");
+    }
+
 }
