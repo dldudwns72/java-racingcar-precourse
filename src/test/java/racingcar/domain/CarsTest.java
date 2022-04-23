5a526a2260cd363f576518eb54054e88d712bc15
dldudwns72 <dldudwns72@gmail.com>
2022-04-23 22:27:24 +0900

    test(Cars) : Cars 도메인 구현 및 예외발생 테스트

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,4 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
 class CarsTest {
-  
+
+    @Test
+    @DisplayName("2대 미만의 자동차 생성 시 예외 발생")
+    void createOneCarsError() {
+        assertThatThrownBy(() -> {
+            new Cars("lee");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[Error] 자동차는 두 대 이상 존재");
+    }
+
+    @Test
+    @DisplayName("중복된 이름 입력 시 예외 발생 ")
+    void validateDuplicate() {
+        assertThatThrownBy(() -> {
+            new Cars("kim,lee,kim");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[Error] 자동차 이름은 중복될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("3대의 자동차 생성")
+    void createCars() {
+        Cars cars = new Cars("lee,kim,park");
+        assertThat(cars.getSize()).isEqualTo(3);
+    }
+
+
 }
