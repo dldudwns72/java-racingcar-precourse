94ee243cdfd8469d9508192b961935ed86aa5e69
dldudwns72 <dldudwns72@gmail.com>
2022-04-24 19:05:29 +0900

    style(application Test) : Application 테스트 통과를 위한 문구 수정

--- src/main/java/racingcar/ui/OutputView.java
+++ src/main/java/racingcar/ui/OutputView.java
@@ -5,13 +5,13 @@ import racingcar.domain.Winners;
 
 public class OutputView {
 
-    private static final String PRINT_WINNERS_PREFIX = "최종 우승자는 ";
-    private static final String PRINT_WINNERS_SUFFIX = " 입니다.";
+    private static final String PRINT_WINNERS_PREFIX = "최종 우승자: ";
 
     public static void finishRacing(Cars cars) {
         Winners winners = new Winners(cars);
         String winnerToString = winners.printWinners();
-        String printWinners = PRINT_WINNERS_PREFIX + winnerToString + PRINT_WINNERS_SUFFIX;
+        String printWinners = PRINT_WINNERS_PREFIX + winnerToString;
         System.out.print(printWinners);
     }
+
 }
